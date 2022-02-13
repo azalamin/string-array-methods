6718be0001c803282bb9e7841605c3a0c0a9949d
Al-amin Sheikh <azalamins@gmail.com>
2022-02-14 03:04:10 +0600

    added splice js

---
+++ app.js
@@ -0,0 +1,9 @@
+let text = "The more efforts you gives, you will be more strong";
+const textSplit = text.split(' ');
+// console.log(textSplit)
+
+const textSlice = text.slice(3, 12);
+// console.log(textSlice)
+
+const textSubstr = text.substr(3, 10);
+console.log(textSubstr)
---
+++ splice.js
@@ -0,0 +1,8 @@
+let numbers = [10, 12, 15, 19, 34, 45, 63, 54, 99];
+// const spliceNumber = numbers.splice(3, 3, 900, 999, 200);
+// console.log(numbers)
+// console.log(spliceNumber)
+
+let spliceNum = numbers.splice(2, 6)
+console.log(numbers)
+console.log(spliceNum)
